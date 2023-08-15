a38b966aba8b7d5849d9989c6729b700a0c1646c
mocha-matcha <sana.ali.said.mohammed@gmail.com>
2023-08-15 00:22:59 -0500

    routing work.

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { User, Post } = require('../../models');
+
+router.get()
+
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.body.user_id,
+    });
+
+    req.session.save(() => {
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
+// Logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars')
 const sequelize = require('./config/connection');
+const path = require('path');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -20,15 +31,16 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-
-sequelize.sync({ force: false }).then(() =>
-
-
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  }))
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
